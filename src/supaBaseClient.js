46eadaaba894d654c073cabe60110d8df471f2b5
R3tr0LastKnight <shubh14shahu@gmail.com>
2024-07-05 14:28:54 +0530

    1.1

--- src/supaBaseClient.js
+++ src/supaBaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const Project_URL = "https://lwlqecmxqvtbmetququq.supabase.co";
-const Anon_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3bHFlY214cXZ0Ym1ldHF1cXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwMDQwNjcsImV4cCI6MjAzNTU4MDA2N30.netQoJf9lHuFoBWFZk0GFnfCTaMjNCoZ3Nh7igXL2-k";
-
-export const supabase = createClient(Project_URL, Anon_KEY);
+export const supabase = createClient(
+  process.env.REACT_APP_Project_URL,
+  process.env.REACT_APP_Anon_KEY
+);
